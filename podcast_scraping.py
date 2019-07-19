63a3f18555de53d36faacce09ca2bf60e5b041b0
Yi Ying <Yi.Ying@suse.com>
2019-07-19 15:27:03 +0800

    add numbers

--- podcast_scraping.py
+++ podcast_scraping.py
@@ -25,20 +25,24 @@ def get_next_url(soup):
         if tag_content == "Main":
             trigger_next = True
 
-def download_files(soup):
+def download_files(soup, num):
     for tag in reversed(soup.findAll("a")):
         tag_content, tag_href = get_tag_contents(tag)
         if re.match(r'^.*\.mp3$', tag_href):
-            print("Downloading", tag_content, "from", tag_href)
-            # wget.download(tag_href, './' + tag_content + ".mp3")
+            print("[{}] Downloading {} from {}".format("{:02}".format(num), tag_content, tag_href))
+            wget.download(tag_href, "./Downloads/[{}] {}.mp3".format("{:02}".format(num), tag_content))
+            print("")
+            num += 1
+    return num
 
 url = "https://www.revolutionspodcast.com/2014/07/index.html"
+num = 1
 while True:
     if url == "https://www.revolutionspodcast.com/2015/12/index.html":
         print("Complete.")
         break
-    print("*****Visiting", url, "*****")
+    print("=============Visiting {}=============".format(url))
     soup = request_page(url)
     url = get_next_url(soup)
-    download_files(soup)
+    num = download_files(soup, num)
     time.sleep(1)
