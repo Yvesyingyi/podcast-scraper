1e1f91b8071ec032e4776e3936ab2a01eeb58096
Yi Ying <Yi.Ying@suse.com>
2019-07-19 15:13:18 +0800

    general rewrite

--- podcast_scraping.py
+++ podcast_scraping.py
@@ -1,38 +1,44 @@
-# Import libraries
 import requests
 import wget
 import time
 from bs4 import BeautifulSoup
 import re
 
-# initial url for french revolution
-url = "https://www.revolutionspodcast.com/2014/07/index.html"
-
-while True:
-    print("Visiting", url)
-    # Connect to the URL
+def request_page(url):
     response = requests.get(url)
-    # Parse HTML and save to BeautifulSoup object
-    soup = BeautifulSoup(response.text, "html.parser")
-    # For identifying next page
+    return BeautifulSoup(response.text, "html.parser")
+
+def get_tag_contents(tag):
+    tag_content = str(tag.string)
+    try:
+        tag_href = tag['href']
+    except:
+        tag_href = ''
+    return tag_content, tag_href
+
+def get_next_url(soup):
     trigger_next = False
-    # Check all <a> tags
     for tag in soup.findAll("a"):
-        # get tag content string, tag href
-        tag_content = str(tag.string)
-        try:
-            tag_href = tag['href']
-        except:
-            tag_href = ''
-        # save next page url
+        tag_content, tag_href = get_tag_contents(tag)
         if trigger_next == True:
-            url = tag_href
-            trigger_next = False
-        # identify next page url
+            return tag_href
         if tag_content == "Main":
             trigger_next = True
-        # download mp3
+
+def download_files(soup):
+    for tag in reversed(soup.findAll("a")):
+        tag_content, tag_href = get_tag_contents(tag)
         if re.match(r'^.*\.mp3$', tag_href):
-            print("Downloading", tag_content, "from", tag_href, "...")
-            wget.download(tag_href, './' + tag_content + ".mp3")
-    time.sleep(1)  # pause the code for a sec
+            print("Downloading", tag_content, "from", tag_href)
+            # wget.download(tag_href, './' + tag_content + ".mp3")
+
+url = "https://www.revolutionspodcast.com/2014/07/index.html"
+while True:
+    if url == "https://www.revolutionspodcast.com/2015/12/index.html":
+        print("Complete.")
+        break
+    print("*****Visiting", url, "*****")
+    soup = request_page(url)
+    url = get_next_url(soup)
+    download_files(soup)
+    time.sleep(1)
